a7789cac30cb26f2c0b9103ee6a5704a930a3137
Juan <juanabca@unisabana.edu.co>
2023-09-21 10:58:17 -0500

    Funcionalidades con API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [visibleUsers, setVisibleUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [showAllUsers, setShowAllUsers] = useState(false);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/users?limit=10`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setVisibleUsers(data.slice(0, 6));
+      });
+  }, []);
+
+  const loadMoreUsers = () => {
+    const startIndex = visibleUsers.length;
+    const endIndex = startIndex + 6;
+    const additionalUsers = users.slice(startIndex, endIndex);
+    setVisibleUsers([...visibleUsers, ...additionalUsers]);
+    setPage(page + 1);
+    setShowAllUsers(true); // Mostrar el botón "Ver más" cuando se cargan más usuarios
+  };
+
+  const toggleShowUsers = () => {
+    if (showAllUsers) {
+      setVisibleUsers(users.slice(0, 6));
+      setShowAllUsers(false); // Ocultar el botón "Ver más" cuando se presiona "Ver menos"
+    } else {
+      setVisibleUsers(users);
+      setShowAllUsers(true); // Mostrar el botón "Ver más" cuando se presiona "Ver menos"
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de Usuarios</h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+        {visibleUsers.map((user) => (
+          <div key={user.id} style={{ width: 'calc(33.33% - 20px)', marginBottom: '20px', border: '1px solid #ccc', padding: '10px', boxSizing: 'border-box' }}>
+            <p>Nombre: {user.name.firstname} {user.name.lastname}</p>
+            <p>Correo: {user.email}</p>
+            <p>User Name: {user.username}</p>
+            <p>Ciudad: {user.address.city}</p>
+            <p>Calle: {user.address.street}</p>
+            <p>Número: {user.address.number}</p>
+            <p>Zipcode: {user.address.zipcode}</p>
+            <p>Teléfono: {user.phone}</p>
+          </div>
+        ))}
+      </div >
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {!showAllUsers && (
+          <button onClick={loadMoreUsers}>Ver Más</button>
+        )}
+        {showAllUsers && (
+          <button onClick={toggleShowUsers}>Ver menos</button>
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
+
